f8d8be5fc37b9056f373ccb5246cf5d8dce8c829
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-03-12 17:39:47 +0530

    Fetched items on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from "react";
 
+type PokemonListItemType = {
+  name: string;
+  url: string;
+}[];
+
+type FetchPokemonsResponseType = {
+  count: 1118;
+  next: string | null;
+  previous: null | null;
+  results: PokemonListItemType;
+};
+
 export default function Home() {
-  const [state, setState] = useState("initialState");
+  const [pokemons, setPokemons] = useState<PokemonListItemType>();
+
   async function fetchPokemons(offset: number, limit: number) {
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     );
-    const data = await res.json();
-    console.log(data);
+    const data = (await res.json()) as FetchPokemonsResponseType;
+    setPokemons(data.results);
   }
 
   useEffect(() => {
     fetchPokemons(0, 10);
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      {pokemons &&
+        pokemons.map((pokemon) => <li key={pokemon.name}>{pokemon.name}</li>)}
+    </div>
+  );
 }

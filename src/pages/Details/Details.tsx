e907a735d8a706d643b96638f32819e51bf6f60c
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2021-03-12 18:39:43 +0530

    Added avatar

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,21 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styles from './Details.module.css';
+import styles from "./Details.module.css";
 
 type ParamsType = { id: string };
 
+type PokemonDetailsType = {
+  abilities: {
+    ability: { name: string; url: string };
+    is_hidden: boolean;
+    slot: number;
+  }[];
+  base_experience: number;
+  forms: { name: string; url: string }[];
+  game_indices: { game_index: 153; version: { name: string; url: string } };
+  height: number;
+  held_items: any[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: any[];
+  name: string;
+  order: number;
+  past_types: any[];
+  species: { name: string; url: string }[];
+  sprites: any;
+  stats: {
+    base_stat: number;
+    effort: number;
+    stat: { name: string; url: string };
+  }[];
+  types: { slot: number; type: { name: string; url: string } }[];
+  weight: number;
+};
+
 export default function Details() {
-  async function fetchPokemons(id: number) {
+  const [pokemon, setPokemon] = useState<PokemonDetailsType>();
+
+  async function fetchPokemonDetails(id: number) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const data = await res.json();
+    const data = (await res.json()) as PokemonDetailsType;
+    setPokemon(data);
     console.log(data);
   }
 
   const { id } = useParams<ParamsType>();
 
   useEffect(() => {
-    if (id) fetchPokemons(Number(id));
+    if (id) fetchPokemonDetails(Number(id));
   }, [id]);
 
-  return <div>Details</div>;
+  if (pokemon) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.pokedetails}>
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            className={styles.avatar}
+          />
+          <h3>{pokemon.name}</h3>
+        </div>
+      </div>
+    );
+  }
+
+  return <div>Details not found</div>;
 }
